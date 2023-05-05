24227d2831b61438652ee6ced5c2d167552b8a0a
Marta <123839132+martapereira0@users.noreply.github.com>
2023-05-05 17:40:10 +0100

    comentários

--- P6/DAA021.cpp
+++ P6/DAA021.cpp
@@ -17,21 +17,12 @@ int main(){
             cin >> energia;
             s.insert(energia);
         }
-        // se nao quiser usar o auto tenho de fazer multiset:: qql coisa
 
         else if(str == "MIN"){
             auto it = s.begin(); // it é como se fosse a posiçao
             cout << *it << endl; // *it é o iterador, acede ao conteúdo
             s.erase(it); // eliminar o conteudo daquela posição
-            // se fizesse s.erase(*it) iria remover todos os "nos" com aquele valor e nós não queremos isso
             
-            
-            // s.rend() aponta para a ultima posição num multiset inverso ou seja para a posição anterior à primeira e como estamos em reverse temos de fazer it--
-            //auto it = s.rend(); // aceder ao primeiro elemento do multiset inverso que equivale ao ultimo elemento do multiset inicial
-            //it--;
-            //cout << *it << endl;
-            //s.erase(it); não é possível apagar elementos numa estrutura reverse, logo nao é possivel fazer erase o mesmo para rbegin() que aponta para o primeiro elemento do multiset inverso ou seja para o ultimo do multiset original
-        
         }
         else{
             auto it = s.end(); // apontar para a posiço exatamente a seguir à ultima
@@ -40,12 +31,6 @@ int main(){
             s.erase(it);
 
         }
-        //cout << "conteudo: ";
-        //for(auto i : s){
-            //cout << i << " ";
-        //}
-        //cout << endl;
-        
     }
     return 0;
 }
